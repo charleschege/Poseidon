a10147386a69cff31cce72e5f41a2e00c88282cb
Wasmium Network Developers <hq@wasmium.network>
2022-01-24 12:45:09 +0300

    Remove redundant code
    Ensure serde renames all types to CamelCase
    
    Deserialize a reponse from calling `getRecentBlockhash` method

--- src/rpc_responses/get_recent_blockhash.rs
+++ src/rpc_responses/get_recent_blockhash.rs
@@ -1,22 +1,39 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use serde::Deserialize;
-/*
-{
-    "jsonrpc": "2.0",
-    "result": {
-        "context": {
-            "slot": 108604506
-        },
-        "value": {
-            "blockhash": "AMKDaoprEUSipYuNM8fgu7azvSXqvHVdwkscUX2yUjHJ",
-            "feeCalculator": {
-                "lamportsPerSignature": 5000
-            }
-        }
-    },
-    "id": 1
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct RecentBlockHashNodeResponse {
+    jsonrpc: String,
+    result: Result,
+    id: Option<u8>,
 }
-*/
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Result {
+    context: Context,
+    value: Value,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Context {
+    slot: u64,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Value {
+    blockhash: String,
+    fee_calculator: FeeCalculator,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct FeeCalculator {
+    lamports_per_signature: u32,
+}
 
 #[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
 pub struct RecentBlockHashResponse {
@@ -31,44 +48,10 @@ impl From<RecentBlockHashNodeResponse> for RecentBlockHashResponse {
     fn from(response: RecentBlockHashNodeResponse) -> Self {
         Self {
             jsonrpc: response.jsonrpc,
-            id: response.id,
+            id: response.id.unwrap(),
             slot: response.result.context.slot,
             blockhash: response.result.value.blockhash,
             lamports_per_signature: response.result.value.fee_calculator.lamports_per_signature,
         }
     }
-}
-
-#[derive(Debug, Clone, Deserialize)]
-#[serde(rename = "camelCase")]
-pub struct RecentBlockHashNodeResponse {
-    jsonrpc: String,
-    result: Result,
-    id: u8,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-#[serde(rename = "camelCase")]
-pub struct Result {
-    context: Context,
-    value: Value,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-#[serde(rename = "camelCase")]
-pub struct Context {
-    slot: u64,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-#[serde(rename = "camelCase")]
-pub struct Value {
-    blockhash: String,
-    fee_calculator: FeeCalculator,
-}
-
-#[derive(Debug, Clone, Deserialize)]
-#[serde(rename = "camelCase")]
-pub struct FeeCalculator {
-    lamports_per_signature: u32,
 }
